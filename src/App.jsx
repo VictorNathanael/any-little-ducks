f387d87758191719e1ed6a6be97ed9732ee08e75
Victor Nathanael <victornathanael15@gmail.com>
2023-06-22 01:09:50 -0300

    feat: add handleChange, handleClick and showOrHidden function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
+import React, { useState } from 'react';
 import './App.css';
+import { LyricsLittleDucks } from './components/LyricsLittleDucks/LyricsLittleDucks';
+
 function App() {
+    const [numberOfDucks, setNumberOfDucks] = useState(0);
+
+    const handleChange = (e) => {
+        const numberOfDucks = e.target.value;
+        setNumberOfDucks(numberOfDucks);
+        showOrHidden('lyrics', 'hidden');
+    };
+
+    const handleClick = () => {
+        if (numberOfDucks >= 1 && numberOfDucks) {
+            showOrHidden('noDucks', 'hidden');
+            showOrHidden('lyrics', 'show');
+        } else {
+            showOrHidden('noDucks', 'show');
+        }
+    };
+
+    function showOrHidden(id, showOrHidden) {
+        if (showOrHidden === 'show') showOrHidden = false;
+        if (showOrHidden === 'hidden') showOrHidden = true;
+        return (document.getElementById(id).hidden = showOrHidden);
+    }
+
     return (
         <div className='App'>
             <h1>Any Little Ducks</h1>
             <div id='input-ducks'>
                 <p>Quantos patinhos foram passear?</p>
-                <input type='number' name='number-of-ducks' />
+                <input
+                    id='quantity-ducks'
+                    type='number'
+                    name='number-of-ducks'
+                    placeholder='Insira um número'
+                    onChange={handleChange}
+                />
+                <button onClick={handleClick}>Confirmar</button>
             </div>
-            <button>confirmar</button>
-            <FiveLittleDucks />
+            <h3 id='noDucks' hidden>
+                Nenhum patinho foi passear :(
+            </h3>
+            <LyricsLittleDucks totalDucks={numberOfDucks} />
         </div>
     );
 }
