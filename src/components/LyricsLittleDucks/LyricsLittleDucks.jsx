6c4ef8004b5805bb9c9389d036aaa556c599c509
Victor Nathanael <victornathanael15@gmail.com>
2023-06-23 10:16:34 -0300

    feat: add parsePlural to change the words to plural

--- src/components/LyricsLittleDucks/LyricsLittleDucks.jsx
+++ src/components/LyricsLittleDucks/LyricsLittleDucks.jsx
@@ -1,31 +1,64 @@
 import React from 'react';
 import './LyricsLittleDucks.css';
 import extenso from 'extenso';
+import listWords from '../../list-words.json';
 
 export function LyricsLittleDucks(props) {
-    let fullNumber;
-    if (props.totalDucks && props.totalDucks > 0) {
-        fullNumber = extenso(props.totalDucks);
-        fullNumber = fullNumber[0].toUpperCase() + fullNumber.substring(1);
+    const totalDucks = Number(props.totalDucks);
+
+    function upFirstLetter(number) {
+        let fullNumber = number;
+        if (number && number > 0) {
+            fullNumber = extenso(number);
+            return (fullNumber =
+                fullNumber[0].toUpperCase() + fullNumber.substring(1));
+        }
     }
+
+    function parsePlural(word, amount) {
+        const index = amount === 1 ? 0 : 1;
+        return listWords.find((item) => item[0] === word)[index];
+    }
+
+    const firstPart = () => {
+        if (totalDucks && totalDucks > 0) {
+            const firstPart = document.getElementById('first-part');
+            let text = '';
+            for (let i = 0; i < totalDucks; i++) {
+                const currentDucks = totalDucks - i;
+                text += `
+                    ${upFirstLetter(currentDucks)} ${parsePlural(
+                    'patinho',
+                    currentDucks
+                )} <br/>
+                ${parsePlural('Foi', currentDucks)} passear <br/>
+                    Além das montanhas <br/>
+                    Para brincar <br/>
+                    <br/>
+                   
+                    `;
+                if (currentDucks !== 1) {
+                    text += ` 
+                    A mamãe gritou <br/>
+                    Quá, quá, quá, quá! <br/>
+                    Mas só ${extenso(currentDucks - 1)} ${parsePlural(
+                        'patinho',
+                        currentDucks - 1
+                    )} <br/>
+                    ${parsePlural('Voltou', currentDucks - 1)} de lá <br/>
+                    <br />
+                    `;
+                }
+            }
+            firstPart.innerHTML = text;
+        }
+    };
 
     return (
         <div className='LyricsLittleDucks'>
             <div id='lyrics' hidden>
-                <p id='first-part'>
-                    {fullNumber} patinhos <br />
-                    Foram passear <br />
-                    Além das montanhas <br />
-                    Para brincar <br />
-                </p>
-                <br />
-                <p id='main-part'>
-                    A mamãe gritou <br />
-                    Quá, quá, quá, quá! <br />
-                    Mas só quatro patinhos <br />
-                    Voltaram de lá <br />
-                </p>
-                <br />
+                <p id='first-part'>{firstPart()}</p>
+
                 <p id='final-part'>
                     A mamãe gritou <br />
                     Quá, quá, quá, quá <br />
@@ -35,7 +68,9 @@ export function LyricsLittleDucks(props) {
                     <br />
                     A mamãe patinha <br />
                     Ficou tão triste naquele dia <br />
-                    Aonde será que estavam seus filhotinhos? <br />
+                    Aonde será que{' '}
+                    {parsePlural('estava seu filhotinho', totalDucks)}
+                    ? <br />
                     Mas essa história vai ter um final feliz <br />
                     Sabe por quê? <br />
                     <br />
@@ -45,9 +80,13 @@ export function LyricsLittleDucks(props) {
                     Na beira do mar <br />
                     <br />
                     A mamãe gritou <br />
-                    Quá, quá, quá, quá! <br />
-                    E os cinco patinhos <br />
-                    Voltaram de lá <br />
+                    Quá, quá, quá, quá! <br />E {parsePlural(
+                        'o',
+                        totalDucks
+                    )}{' '}
+                    {totalDucks > 1 ? extenso(totalDucks) : ''}
+                    {parsePlural('patinho', totalDucks)} <br />
+                    {parsePlural('Voltou', totalDucks)} de lá <br />
                 </p>
             </div>
         </div>
