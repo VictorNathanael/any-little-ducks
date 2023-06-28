d343caec660a0d50e791d005e1e0c2e8beaf0e6e
Victor Nathanael <victornathanael15@gmail.com>
2023-06-28 09:09:21 -0300

    feat: add pageFeedbackTypes for update result or error

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,60 @@
 import React, { useState } from 'react';
 import './App.css';
 import { LyricsLittleDucks } from './components/LyricsLittleDucks/LyricsLittleDucks';
+import { debounce } from 'lodash';
+
+const pageFeedbackTypes = {
+    none: 'none',
+    result: 'result',
+    error: 'error',
+};
 
 function App() {
     const [quantityDucks, setQuantityDucks] = useState(0);
-    const [showResult, setShowResult] = useState(false);
-    const [showError, setShowError] = useState(false);
+    const [pageFeedback, setPageFeedback] = useState(pageFeedbackTypes.none);
 
-    const handleChange = (e) => {
+    const handleInputChange = (e) => {
         if (!e.target.value.includes('e')) {
             setQuantityDucks(e.target.value);
-            setShowResult(false);
-            setShowError(false);
+            setPageFeedback(pageFeedbackTypes.none);
         }
     };
 
+    const debouncedHandleInputChange = debounce(handleInputChange, 300);
+
     const handleClick = () => {
         if (quantityDucks >= 1) {
-            setShowResult(true);
+            setPageFeedback(pageFeedbackTypes.result);
         } else {
-            setShowError(true);
+            setPageFeedback(pageFeedbackTypes.error);
+        }
+    };
+
+    const buildContent = () => {
+        switch (pageFeedback) {
+            case pageFeedbackTypes.error:
+                return <h3 id='noDucks'>Nenhum patinho foi passear :(</h3>;
+            case pageFeedbackTypes.result:
+                return <LyricsLittleDucks totalDucks={quantityDucks} />;
+            default:
+                return null;
         }
     };
 
     return (
         <div className='App'>
-            <h1>Any Little Ducks</h1>
+            <h1>{'Any Little Ducks'}</h1>
             <div id='input-ducks'>
                 <p>Quantos patinhos foram passear?</p>
                 <input
                     id='quantity-ducks'
                     type='number'
                     placeholder='Insira um número'
-                    onChange={handleChange}
+                    onChange={debouncedHandleInputChange}
                 />
                 <button onClick={handleClick}>Confirmar</button>
             </div>
-            {showError && <h3 id='noDucks'>Nenhum patinho foi passear :(</h3>}
-            <LyricsLittleDucks
-                totalDucks={quantityDucks}
-                showResult={showResult}
-            />
+            {buildContent()}
         </div>
     );
 }
