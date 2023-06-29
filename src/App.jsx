f3120459ab2a68f43971bd4fa47aaee1c30fa969
Victor Nathanael <victornathanael15@gmail.com>
2023-06-29 17:00:53 -0300

    add inputError, duck image and update input for NumericFormat

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
-import { LyricsLittleDucks } from './components/LyricsLittleDucks/LyricsLittleDucks';
+import LyricsLittleDucks from './components/lyricsLittleDucks/LyricsLittleDucks';
 import { debounce } from 'lodash';
+import patinhoChorando from './assets/patinho-chorando.png';
+import { NumericFormat } from 'react-number-format';
 
 const pageFeedbackTypes = {
     none: 'none',
@@ -12,46 +14,61 @@ const pageFeedbackTypes = {
 function App() {
     const [quantityDucks, setQuantityDucks] = useState(0);
     const [pageFeedback, setPageFeedback] = useState(pageFeedbackTypes.none);
+    const [inputError, setInputError] = useState(false);
 
     const handleInputChange = (e) => {
-        if (!e.target.value.includes('e')) {
-            setQuantityDucks(e.target.value);
-            setPageFeedback(pageFeedbackTypes.none);
-        }
+        setQuantityDucks(e.target.value);
+        setPageFeedback(pageFeedbackTypes.none);
+        setInputError(false);
     };
 
-    const debouncedHandleInputChange = debounce(handleInputChange, 300);
+    const handleDebouncedInputChange = debounce(handleInputChange, 300);
 
     const handleClick = () => {
-        if (quantityDucks >= 1) {
+        if (quantityDucks >= 1 && quantityDucks <= 100000) {
             setPageFeedback(pageFeedbackTypes.result);
         } else {
             setPageFeedback(pageFeedbackTypes.error);
+            setInputError(true);
         }
     };
 
-    const buildContent = () => {
+    function buildContent() {
         switch (pageFeedback) {
             case pageFeedbackTypes.error:
-                return <h3 id='noDucks'>Nenhum patinho foi passear :(</h3>;
+                return (
+                    <div>
+                        <img
+                            src={patinhoChorando}
+                            alt='imagem de um patinho amarelo chorando'
+                            width={'180px'}
+                        />
+                    </div>
+                );
             case pageFeedbackTypes.result:
                 return <LyricsLittleDucks totalDucks={quantityDucks} />;
             default:
                 return null;
         }
-    };
+    }
 
     return (
         <div className='App'>
             <h1>{'Any Little Ducks'}</h1>
-            <div id='input-ducks'>
+            <div className='container-input'>
                 <p>Quantos patinhos foram passear?</p>
-                <input
-                    id='quantity-ducks'
+                <NumericFormat
+                    className={inputError ? 'input-error' : ''}
                     type='number'
-                    placeholder='Insira um número'
-                    onChange={debouncedHandleInputChange}
+                    onChange={handleDebouncedInputChange}
+                    allowedDecimalSeparators={false}
+                    decimalScale={0}
                 />
+                {inputError && (
+                    <p className={'text-error'}>
+                        Digite um número de 1 a 100.000
+                    </p>
+                )}
                 <button onClick={handleClick}>Confirmar</button>
             </div>
             {buildContent()}
